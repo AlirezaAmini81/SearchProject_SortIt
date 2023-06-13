a3d2d6f884fc863367c1902ab4998769344fe8a2
Alireza <you@example.com>
2023-06-13 17:11:48 +0330

    Add DFS, IDS, UCS, A_Star, IDA_Star search Algorithms with heuristic function

--- Search.py
+++ Search.py
@@ -1,21 +1,120 @@
+
+from queue import PriorityQueue
 from Solution import Solution
 from Problem import Problem
 from datetime import datetime
 
 
+
 class Search:
     @staticmethod
-    def bfs(prb: Problem) -> Solution:  # this method get a first state of Problem and do bfs for find solution if no
+    def dfs(prb: Problem) -> Solution:  # this method get a first state of Problem and do bfs for find solution if no
         # solution is find return None else return the solution
+        explored = {}
         start_time = datetime.now()
-        queue = []
+        stack = []
         state = prb.initState
-        queue.append(state)
-        while len(queue) > 0:
-            state = queue.pop(0)
+        stack.append(state)
+        while len(stack) > 0:
+            state = stack.pop(0)
+            explored[state.__hash__()] = 1
+            neighbors = prb.successor(state)
+            for c in neighbors:
+                if c.__hash__() not in explored.keys():
+                    if prb.is_goal(c):
+                        print(c.g_n)
+                        return Solution(c, prb, start_time)
+                    stack.append(c)
+        return None
+    def ids(prb: Problem) -> Solution:  # this method get a first state of Problem and do bfs for find solution if no
+        # solution is find return None else return the solution
+        for limit in range(0,10000):
+            print(limit)
+            explored = {}
+            start_time = datetime.now()
+            stack = []
+            state = prb.initState
+            stack.append(state)
+            while len(stack) > 0:
+                state = stack.pop(-1)
+                explored[state.__hash__()] = 1
+                neighbors = prb.successor(state)
+                for c in neighbors:
+                    if c.__hash__() not in explored.keys():
+                        if prb.is_goal(c):
+                            print(c.g_n)
+                            return Solution(c, prb, start_time)
+                        if state.g_n <= limit:
+                            stack.append(c)
+        return None
+
+
+
+    def ucs(prb: Problem) -> Solution:  # this method get a first state of Problem and do bfs for find solution if no
+        # solution is find return None else return the solution
+        explored = {}
+        start_time = datetime.now()
+        hq = PriorityQueue()
+        state = prb.initState
+        hq.put((state.cost, state))
+        while not hq.empty():
+            state = hq.get()[1]
+            explored[state.__hash__()] = state
             neighbors = prb.successor(state)
             for c in neighbors:
                 if prb.is_goal(c):
                     return Solution(c, prb, start_time)
-                queue.append(c)
+                elif c.__hash__() not in explored.keys():
+                    hq.put((c.cost, c))
         return None
+
+    def A_star(prb: Problem) -> Solution:  # this method get a first state of Problem and do bfs for find solution if no
+        # solution is find return None else return the solution
+        explored = {}
+        start_time = datetime.now()
+        hq = PriorityQueue()
+        state = prb.initState
+        hq.put((state.g_n + state.h(), state))
+        print(state.g_n + state.h())
+        while not hq.empty():
+            state = hq.get()[1]
+            explored[state.__hash__()] = state
+            neighbors = prb.successor(state)
+            for c in neighbors:
+                if prb.is_goal(c):
+                    print(c.g_n)
+                    return Solution(c, prb, start_time)
+                elif c.__hash__() not in explored.keys():
+                    hq.put((c.g_n + c.h(), c))
+        return None
+
+    def IDA_star(prb: Problem) -> Solution:  # this method get a first state of Problem and do bfs for find solution if no
+        # solution is find return None else return the solution
+        q = []
+        q.append(prb.initState.g_n + prb.initState.h())
+        while True:
+            cut_off = min(q)
+            print(cut_off)
+            q = []
+            explored = {}
+            start_time = datetime.now()
+            stack = []
+            state = prb.initState
+            stack.append(state)
+            while len(stack) > 0:
+                state = stack.pop(-1)
+                explored[state.__hash__()] = 1
+                neighbors = prb.successor(state)
+                for c in neighbors:
+                    if prb.is_goal(c):
+                        print(c.g_n)
+                        return Solution(c, prb, start_time)
+                    if c.__hash__() not in explored.keys():
+                        if c.g_n + c.h() <= cut_off:
+                            stack.append(c)
+                        elif c.g_n + c.h() > cut_off:
+                            q.append(c.g_n + c.h())
+        return None
+
+
+
--- State.py
+++ State.py
@@ -5,6 +5,7 @@ class State:
         self.parent = parent
         self.g_n = g_n
         self.prev_action = prev_action
+        self.set_cost()
 
     def change_between_two_pipe(self, pipe_src_ind: int, pipe_dest_ind: int):
         self.pipes[pipe_dest_ind].add_ball(self.pipes[pipe_src_ind].remove_ball())
@@ -18,3 +19,26 @@ class State:
         for i in hash_strings:
             hash_string += i + '###'
         return hash_string
+
+    def set_cost(self):
+        if self.parent is not None:
+            self.cost = self.parent.cost + abs(self.prev_action[0] - self.prev_action[1])
+        else:
+            self.cost = 0
+
+    def __lt__(self, other):
+        return 1
+
+    def h(self):
+        count = 0
+        for i in self.pipes:
+            counttmp = 1
+            if not i.is_empty():
+                color = i.stack[0]
+                for j in range(1, len(i.stack)):
+                    if i.stack[j] == color:
+                        counttmp += 1
+                    else:
+                        break
+                count += (counttmp ** 2)
+        return -count
